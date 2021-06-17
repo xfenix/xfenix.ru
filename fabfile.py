b84e2818a09fd02bf04a774ca62296e8c758d3ac
Denis Anikin <ad@xfenix.ru>
2021-06-17 04:12:57 +0300

    Deploy updated

--- fabfile.py
+++ fabfile.py
@@ -9,11 +9,6 @@ BACK_DIR: str = f"{PROJECT_DIR}back/"
 SITECON: Connection = Connection(f'{os.getenv("XFENIXRU_USER")}@xfenix.ru', port=os.getenv("XFENIXRU_PORT"))
 
 
-def restart_back():
-    with SITECON.cd(BACK_DIR):
-        SITECON.run(f"npx pm2 restart server.js")
-
-
 @task
 def deploy(context):
     print("Running deploy...")
@@ -25,7 +20,9 @@ def deploy(context):
 def deployfull(context):
     print("Running full deploy...")
     SITECON.run(f"cd {PROJECT_DIR} && git pull")
-    restart_back()
+    with SITECON.cd(BACK_DIR):
+        SITECON.run(f"npm i")
+        SITECON.run(f"npx pm2 restart server.js")
     print("Done!")
 
 
@@ -35,12 +32,4 @@ def install(context):
     SITECON.run(f"cd {PROJECT_DIR} && git pull")
     with SITECON.cd(BACK_DIR):
         SITECON.run(f"npm i")
-    print("Done!")
-
-
-@task
-def clean_cache(context):
-    print("Clean cache")
-    SITECON.run(f"cd {PROJECT_DIR}/back/ && rm store.json 2> /dev/null")
-    restart_back()
     print("Done!")
