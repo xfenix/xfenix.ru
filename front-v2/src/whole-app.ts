4359769bcab2d3f6be0cfb319588fa774b0c7087
Denis Anikin <ad@xfenix.ru>
2021-11-12 05:46:36 +0300

    More improvements

--- front-v2/src/whole-app.ts
+++ front-v2/src/whole-app.ts
@@ -1,11 +1,14 @@
 import $ from "cash-dom";
 
-// const header = $(".top-head");
-// window.onscroll = () => {
-//     console.log(window.pageYOffset);
-//     if (window.pageYOffset > 0) {
-//         header.addClass("top-head_sticky");
-//     } else {
-//         header.removeClass("top-head_sticky");
-//     }
-// }
+for (const oneGroup of ['.skills-switch__tab', '.skills-group']) {
+    $(oneGroup).each(function (index: number) {
+        $(this).attr('data-index', String(index));
+    });
+}
+
+$('.skills-switch__tab').on('click', function(event: Event) {
+    event.preventDefault();
+    $('.skills-switch__tab.active,.skills-group.active').removeClass('active');
+    $(this).addClass('active');
+    $(`.skills-group[data-index="${$(this).data('index')}"]`).addClass('active');
+});
