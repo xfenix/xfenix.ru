d10f30e173ede51de1be6ac92c5e067f372dd3ec
Denis Anikin <ad@xfenix.ru>
2021-11-13 06:15:26 +0300

    More progress

--- front-v2/src/whole-app.ts
+++ front-v2/src/whole-app.ts
@@ -1,14 +1,25 @@
 import $ from "cash-dom";
 
+// skills tabs
 for (const oneGroup of ['.skills-switch__tab', '.skills-group']) {
     $(oneGroup).each(function (index: number) {
         $(this).attr('data-index', String(index));
     });
 }
 
-$('.skills-switch__tab').on('click', function(event: Event) {
-    event.preventDefault();
+$('.skills-switch__tab').on('click', function(eventObj: Event) {
+    eventObj.preventDefault();
     $('.skills-switch__tab.active,.skills-group.active').removeClass('active');
     $(this).addClass('active');
     $(`.skills-group[data-index="${$(this).data('index')}"]`).addClass('active');
+});
+
+// scrolls support
+$('.top-head__menulink, .top-head__logolink').on('click', function (eventObj: Event) {
+    eventObj.preventDefault();
+    const originalHref: string = $(this).attr('href');
+    window.scrollTo({
+        top: originalHref.replace('#', '') ? $(originalHref).offset().top - $('.top-head').height() : 0,
+        behavior: 'smooth'
+    });
 });
