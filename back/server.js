f9b89855d1b7b9056996d5a861bb0fbd31b5ca35
Denis Anikin <ad@xfenix.ru>
2021-11-16 05:30:42 +0300

    Rebuild server

--- back/server.js
+++ back/server.js
@@ -54,7 +54,7 @@ fastify.get('/api/githubrepos/', async (_, serverReply) => {
     await serverReply;
 })
 
-fastify.listen(APP_PORT, (err, address) => {
+fastify.listen(APP_PORT, '0.0.0.0', (err, address) => {
     if (err) throw err;
     fastify.log.info(`server listening on ${address}`);
 })
