96f8799591ffd61eca097e22a3d62a997bb10a8c
Denis Anikin <ad@xfenix.ru>
2022-05-17 03:00:39 +0300

    More typescript in... you now, typescript file. And remove comments from html by default

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
+const gulp = require("gulp");
 // some css & html things
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
-const cssMinify = require('cssnano');
-const htmlmin = require('gulp-htmlmin');
-const htmlValidator = require('gulp-html');
-const minifyInline = require('gulp-minify-inline');
-const minifyInlineJSON = require('gulp-minify-inline-json');
-const uncache = require('gulp-uncache');
-const typograf = require('gulp-typograf');
-const autoprefixer = require('gulp-autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const jsonminify = require('gulp-jsonminify');
-const svgminify = require('gulp-svgmin');
-const pleaseReplace = require('gulp-replace');
+const cssMinify = require("cssnano");
+const htmlmin = require("gulp-htmlmin");
+const htmlValidator = require("gulp-html");
+const minifyInline = require("gulp-minify-inline");
+const minifyInlineJSON = require("gulp-minify-inline-json");
+const uncache = require("gulp-uncache");
+const typograf = require("gulp-typograf");
+const autoprefixer = require("gulp-autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+const jsonminify = require("gulp-jsonminify");
+const svgminify = require("gulp-svgmin");
+const pleaseReplace = require("gulp-replace");
 // typescript things
 const browserify = require("browserify");
 const tsify = require("tsify");
@@ -24,140 +24,159 @@ const buffer = require("vinyl-buffer");
 const uglify = require("gulp-uglify-es").default;
 const sourcemaps = require("gulp-sourcemaps");
 // and something other
-const browserSync = require('browser-sync').create();
-const spawnProc = require('child_process').spawn;
+const browserSync = require("browser-sync").create();
+const spawnProc = require("child_process").spawn;
 // configs
 const ROOT_DIR = `${__dirname}/src`;
-const DESTINATION_DIR =  `${ROOT_DIR}/build`;
+const DESTINATION_DIR = `${ROOT_DIR}/build`;
 const PATTERNS = {
-    html: `${ROOT_DIR}/*.html`,
-    sass: `${ROOT_DIR}/*.scss`,
-    js: `${ROOT_DIR}/*.js`,
-    ts: `${ROOT_DIR}/*.ts`,
-    assets: `${ROOT_DIR}/assets/**`,
+  html: `${ROOT_DIR}/*.html`,
+  sass: `${ROOT_DIR}/*.scss`,
+  js: `${ROOT_DIR}/*.js`,
+  ts: `${ROOT_DIR}/*.ts`,
+  assets: `${ROOT_DIR}/assets/**`,
 };
 
-gulp.task('process-styles', () => {
-    return gulp.src(PATTERNS.sass)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(autoprefixer())
-        .pipe(
-            postcss([
-                postcssPresetEnv(),
-                cssMinify({
-                    discardComments: {
-                        removeAll: true,
-                    },
-                }),
-            ])
-        )
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('process-ts', function () {
-    return browserify({
-            basedir: ".",
-            debug: false,
-            entries: ["src/whole-app.ts"],
-            cache: {},
-            packageCache: {},
-        })
-        .plugin(tsify, {
-            esModuleInterop: true
-        })
-        .transform("babelify", {
-            presets: ["@babel/preset-env"],
-            extensions: [".ts"],
-        })
-        .bundle()
-        .pipe(vinylSource("whole-app.js"))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('process-html', () => {
-    let basicStream = gulp.src(PATTERNS.html);
-    if (process.env.DEVEL) {
-        basicStream = basicStream.pipe(htmlValidator());
-    }
-    return basicStream
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeRedundantAttributes: true,
-            sortAttributes: true,
-            sortClassName: true,
-        }))
-        .pipe(typograf({
-            locale: ['ru', 'en-US'],
-            htmlEntity: { type: 'name' }
-        }))
-        .pipe(minifyInline())
-        .pipe(minifyInlineJSON())
-        .pipe(pleaseReplace("</li>", ""))
-        .pipe(pleaseReplace("</p>", ""))
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('process-uncache', () => {
-    return gulp.src(DESTINATION_DIR + '/*.html')
-        .pipe(uncache({
-            append: 'hash',
-            srcDir: DESTINATION_DIR,
-            distDir: DESTINATION_DIR
-        }))
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('copy-assets', () => {
-    return gulp.src(PATTERNS.assets)
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('minify-json-assets', () => {
-    return gulp.src(DESTINATION_DIR + '/*.json')
-        .pipe(jsonminify())
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('minify-svg-assets', () => {
-    return gulp.src(DESTINATION_DIR + '/*.svg')
-        .pipe(svgminify())
-        .pipe(gulp.dest(DESTINATION_DIR));
-});
-
-gulp.task('process-assets', gulp.series(
-    'copy-assets',
-    gulp.parallel(
-        'minify-json-assets',
-        'minify-svg-assets'
+gulp.task("process-styles", () => {
+  return gulp
+    .src(PATTERNS.sass)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(
+      postcss([
+        postcssPresetEnv(),
+        cssMinify({
+          discardComments: {
+            removeAll: true,
+          },
+        }),
+      ])
     )
-));
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("process-ts", function () {
+  return browserify({
+    basedir: ".",
+    debug: false,
+    entries: ["src/whole-app.ts"],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify, {
+      esModuleInterop: true,
+    })
+    .transform("babelify", {
+      presets: ["@babel/preset-env"],
+      extensions: [".ts"],
+    })
+    .bundle()
+    .pipe(vinylSource("whole-app.js"))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("process-html", () => {
+  let basicStream = gulp.src(PATTERNS.html);
+  if (process.env.DEVEL) {
+    basicStream = basicStream.pipe(htmlValidator());
+  }
+  return basicStream
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        sortAttributes: true,
+        sortClassName: true,
+        removeComments: true,
+      })
+    )
+    .pipe(
+      typograf({
+        locale: ["ru", "en-US"],
+        htmlEntity: { type: "name" },
+      })
+    )
+    .pipe(minifyInline())
+    .pipe(minifyInlineJSON())
+    .pipe(pleaseReplace("</li>", ""))
+    .pipe(pleaseReplace("</p>", ""))
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("process-uncache", () => {
+  return gulp
+    .src(DESTINATION_DIR + "/*.html")
+    .pipe(
+      uncache({
+        append: "hash",
+        srcDir: DESTINATION_DIR,
+        distDir: DESTINATION_DIR,
+      })
+    )
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("copy-assets", () => {
+  return gulp.src(PATTERNS.assets).pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("minify-json-assets", () => {
+  return gulp
+    .src(DESTINATION_DIR + "/*.json")
+    .pipe(jsonminify())
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task("minify-svg-assets", () => {
+  return gulp
+    .src(DESTINATION_DIR + "/*.svg")
+    .pipe(svgminify())
+    .pipe(gulp.dest(DESTINATION_DIR));
+});
+
+gulp.task(
+  "process-assets",
+  gulp.series(
+    "copy-assets",
+    gulp.parallel("minify-json-assets", "minify-svg-assets")
+  )
+);
 
 // Tasks for users starts here
-gulp.task('build', gulp.series(
+gulp.task(
+  "build",
+  gulp.series(
     gulp.parallel(
-        'process-ts',
-        'process-styles',
-        'process-html',
-        'process-assets'
+      "process-ts",
+      "process-styles",
+      "process-html",
+      "process-assets"
     ),
-    'process-uncache'
-));
+    "process-uncache"
+  )
+);
 
-gulp.task('watch', (cb) => {
-    process.env.DEVEL = true;
-    browserSync.init({
-        server: {
-            baseDir: DESTINATION_DIR
-        }
-    });
-    spawnProc("node", ["../back/server.js"], { stdio: "inherit", env: {...process.env, ...{DEBUG: 1}}});
-    gulp.watch(PATTERNS.sass, gulp.series('process-styles', 'process-html', 'process-uncache'));
-    gulp.watch(PATTERNS.html, gulp.series('process-html', 'process-uncache'));
-    gulp.watch(PATTERNS.ts, gulp.series('process-ts', 'process-uncache'));
-    gulp.watch(PATTERNS.assets, gulp.series('process-assets'));
-    cb();
+gulp.task("watch", (cb) => {
+  process.env.DEVEL = true;
+  browserSync.init({
+    server: {
+      baseDir: DESTINATION_DIR,
+    },
+  });
+  spawnProc("node", ["../back/server.js"], {
+    stdio: "inherit",
+    env: { ...process.env, ...{ DEBUG: 1 } },
+  });
+  gulp.watch(
+    PATTERNS.sass,
+    gulp.series("process-styles", "process-html", "process-uncache")
+  );
+  gulp.watch(PATTERNS.html, gulp.series("process-html", "process-uncache"));
+  gulp.watch(PATTERNS.ts, gulp.series("process-ts", "process-uncache"));
+  gulp.watch(PATTERNS.assets, gulp.series("process-assets"));
+  cb();
 });
--- front/src/whole-app.ts
+++ front/src/whole-app.ts
@@ -1,138 +1,194 @@
-import $ from "cash-dom";
+import $, { Cash } from "cash-dom";
 import LazyLoad from "vanilla-lazyload";
-import smoothscroll from 'smoothscroll-polyfill';
+import smoothscroll from "smoothscroll-polyfill";
+
+// warn about old ie (before all other things)
+if (/Trident\/|MSIE/.test(window.navigator.userAgent)) {
+  $("body").addClass("outdated");
+}
 
 // initial things
 smoothscroll.polyfill();
-(new LazyLoad()).update();
+new LazyLoad().update();
 
 // burger animation + on click improvements
-const $burger = $('.burger');
-const $topHeadMenu = $('.top-head__aside');
+const $burger: Cash = $(".burger");
+const $topHeadMenu: Cash = $(".top-head__aside");
 
-$burger.on('click', function (eventObj: Event) {
-    eventObj.preventDefault();
-    $(this).toggleClass('active');
-    $topHeadMenu.toggleClass('active');
+$burger.on("click", function (eventObj: Event) {
+  eventObj.preventDefault();
+  $(this).toggleClass("active");
+  $topHeadMenu.toggleClass("active");
 });
 
-$('.top-head__menulink').on('click', function (eventObj: Event) {
-    eventObj.preventDefault();
-    if ($burger.hasClass('active')) {
-        $burger.removeClass('active');
-        $topHeadMenu.removeClass('active');
-    }
+$(".top-head__menulink").on("click", function (eventObj: Event) {
+  eventObj.preventDefault();
+  if ($burger.hasClass("active")) {
+    $burger.removeClass("active");
+    $topHeadMenu.removeClass("active");
+  }
 });
 
 // skills tabs
-['.skills-switch__tab', '.skills-group'].map((oneGroup: string) => {
-    $(oneGroup).each(function (index: number) {
-        $(this).attr('data-index', String(index));
-    });
+[".skills-switch__tab", ".skills-group"].map((oneGroup: string) => {
+  $(oneGroup).each(function (index: number) {
+    $(this).attr("data-index", String(index));
+  });
 });
 
-$('.skills-switch__tab').on('click', function(eventObj: Event) {
-    eventObj.preventDefault();
-    $('.skills-switch__tab.active,.skills-group.active').removeClass('active');
-    $(this).addClass('active');
-    $(`.skills-group[data-index="${$(this).data('index')}"]`).addClass('active');
+$(".skills-switch__tab").on("click", function (eventObj: Event) {
+  eventObj.preventDefault();
+  $(".skills-switch__tab.active,.skills-group.active").removeClass("active");
+  $(this).addClass("active");
+  $(`.skills-group[data-index="${$(this).data("index")}"]`).addClass("active");
 });
 
 // scrolls support
-$('.top-head__menulink, .top-head__logolink').on('click', function (eventObj: Event) {
+$(".top-head__menulink, .top-head__logolink").on(
+  "click",
+  function (eventObj: Event) {
     eventObj.preventDefault();
-    const originalHref: string = $(this).attr('href');
+    const originalHref: string = $(this).attr("href");
     window.scrollTo({
-        top: originalHref.replace('#', '') ? $(originalHref).offset().top - $('.top-head').height() : 0,
-        behavior: 'smooth'
+      top: originalHref.replace("#", "")
+        ? $(originalHref).offset().top - $(".top-head").height()
+        : 0,
+      behavior: "smooth",
     });
-});
+  }
+);
 
-// email obfuscation
-$('.contacts__link_email').one('click', function () {
-    const rot13New = (inputStr) => {
-        return inputStr.replace(/[A-Z]/gi, char =>
-            "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(char)]);
-    };
-    $(this).attr('href',
-        rot13New('znvygb:') + rot13New('nq') +
-        rot13New('--ksravk-eh').replace('--', '@').replace('-', '.'));
+// email obfuscation (hard-coded as hell)
+$(".contacts__link_email").one("click", function () {
+  const rot13New = (inputStr: string) => {
+    return inputStr.replace(
+      /[A-Z]/gi,
+      (char: string) =>
+        "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(char)
+        ]
+    );
+  };
+  $(this).attr(
+    "href",
+    rot13New("znvygb:") +
+      rot13New("nq") +
+      rot13New("--ksravk-eh").replace("--", "@").replace("-", ".")
+  );
 });
-
-// warn about old ie
-if (/Trident\/|MSIE/.test(window.navigator.userAgent)) {
-    $('body').addClass('outdated');
-}
 
 // github repos rendering with cache
-const CACHE_KEY = 'xfenix-github-cache';
-const API_KEY = 'xfenix-apiaddr';
-const BYPASS_KEY = 'xfenix-bypasscache';
+const CACHE_KEY = "xfenix-github-cache";
+const API_KEY = "xfenix-apiaddr";
+const BYPASS_KEY = "xfenix-bypasscache";
 const GITHUB_CACHE_SECONDS = 12 * 3600;
 const SHOW_REPOS = 6;
-const API_DESTINATION = localStorage.getItem(API_KEY) ? localStorage.getItem(API_KEY) : '/api/githubrepos/';
+const API_DESTINATION = localStorage.getItem(API_KEY)
+  ? localStorage.getItem(API_KEY)
+  : "/api/githubrepos/";
 const localStorageWithExpiration = {
-    set: (cacheKey, inputValue, ttlSeconds): void => {
-        localStorage.setItem(cacheKey.toString(), JSON.stringify({
-            value: inputValue, expires_at: new Date().getTime() + (ttlSeconds * 1000) / 1
-        }));
-    },
-    get: (cacheKey): Object | null => {
-        if (localStorage.getItem(BYPASS_KEY)) {
-            return null;
-        }
-        const storageData = JSON.parse(localStorage.getItem(cacheKey.toString()));
-        if (storageData !== null) {
-            if (storageData.expires_at !== null && storageData.expires_at < new Date().getTime()) {
-                localStorage.removeItem(cacheKey.toString());
-            } else {
-                return storageData.value;
-            }
-        }
-        return null;
+  set: (cacheKey: string, inputValue: Object, ttlSeconds: number): void => {
+    localStorage.setItem(
+      cacheKey.toString(),
+      JSON.stringify({
+        value: inputValue,
+        expires_at: new Date().getTime() + (ttlSeconds * 1000) / 1,
+      })
+    );
+  },
+  get: (cacheKey: string): Object | null => {
+    if (localStorage.getItem(BYPASS_KEY)) {
+      return null;
     }
+    const storageData: { value: Object; expires_at: number } = JSON.parse(
+      localStorage.getItem(cacheKey.toString())
+    );
+    if (storageData !== null) {
+      if (
+        storageData.expires_at !== null &&
+        storageData.expires_at < new Date().getTime()
+      ) {
+        localStorage.removeItem(cacheKey.toString());
+      } else {
+        return storageData.value;
+      }
+    }
+    return null;
+  },
 };
 const cachedPayload = localStorageWithExpiration.get(CACHE_KEY);
-const destinationNode = document.querySelector('.github-wannabe');
-const templateElement: HTMLTemplateElement = document.querySelector('#github-wannabe-tpl');
-const renderOneRepo = (oneRepoPayload) => {
-    const newNode: HTMLElement = templateElement.content.cloneNode(true) as HTMLElement;
-    const titleNode: HTMLAnchorElement = newNode.querySelector('.github-wannabe__title > a');
-    const starsNode: HTMLElement = newNode.querySelector('.github-wannabe__stars');
-    const forkNode: HTMLElement = newNode.querySelector('.github-wannabe__fork');
-    const languageNode: HTMLElement = newNode.querySelector('.github-wannabe__language');
-    titleNode.textContent = oneRepoPayload.name;
-    titleNode.href = oneRepoPayload.html_url;
-    newNode.querySelector('.github-wannabe__descr').textContent = oneRepoPayload.description;
-    starsNode.textContent = oneRepoPayload.stargazers_count;
-    (starsNode.parentNode as HTMLAnchorElement).href = `${oneRepoPayload.html_url}/stargazers`;
-    forkNode.textContent = oneRepoPayload.forks_count;
-    (forkNode.parentNode as HTMLAnchorElement).href = `${oneRepoPayload.html_url}/network/members`;
-    languageNode.querySelector('.github-repo-color')
-        .classList.add('github-repo-color_' + (oneRepoPayload.language ? oneRepoPayload.language : 'nolanguage').toLowerCase());
-    languageNode.appendChild(document.createTextNode(oneRepoPayload.language ? oneRepoPayload.language : 'No language'));
-    return newNode;
+const destinationNode = document.querySelector(".github-wannabe");
+const templateElement: HTMLTemplateElement = document.querySelector(
+  "#github-wannabe-tpl"
+);
+const renderOneRepo = (oneRepoPayload: {
+  name: string;
+  html_url: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  language: string;
+}) => {
+  const newNode: HTMLElement = templateElement.content.cloneNode(
+    true
+  ) as HTMLElement;
+  const titleNode: HTMLAnchorElement = newNode.querySelector(
+    ".github-wannabe__title > a"
+  );
+  const starsNode: HTMLElement = newNode.querySelector(
+    ".github-wannabe__stars"
+  );
+  const forkNode: HTMLElement = newNode.querySelector(".github-wannabe__fork");
+  const languageNode: HTMLElement = newNode.querySelector(
+    ".github-wannabe__language"
+  );
+  titleNode.textContent = oneRepoPayload.name;
+  titleNode.href = oneRepoPayload.html_url;
+  newNode.querySelector(".github-wannabe__descr").textContent =
+    oneRepoPayload.description;
+  starsNode.textContent = String(oneRepoPayload.stargazers_count);
+  (
+    starsNode.parentNode as HTMLAnchorElement
+  ).href = `${oneRepoPayload.html_url}/stargazers`;
+  forkNode.textContent = String(oneRepoPayload.forks_count);
+  (
+    forkNode.parentNode as HTMLAnchorElement
+  ).href = `${oneRepoPayload.html_url}/network/members`;
+  languageNode
+    .querySelector(".github-repo-color")
+    .classList.add(
+      "github-repo-color_" +
+        (oneRepoPayload.language
+          ? oneRepoPayload.language
+          : "nolanguage"
+        ).toLowerCase()
+    );
+  languageNode.appendChild(
+    document.createTextNode(
+      oneRepoPayload.language ? oneRepoPayload.language : "No language"
+    )
+  );
+  return newNode;
 };
 const renderMultipleRepo = (multiplePayload) => {
-    const tmpFragment = document.createDocumentFragment();
-    let counter = 1;
-    for (const oneRepoPayload of multiplePayload) {
-        tmpFragment.appendChild(renderOneRepo(oneRepoPayload));
-        counter++;
-        if (counter > SHOW_REPOS) {
-            break;
-        }
+  const tmpFragment = document.createDocumentFragment();
+  let counter = 1;
+  for (const oneRepoPayload of multiplePayload) {
+    tmpFragment.appendChild(renderOneRepo(oneRepoPayload));
+    counter++;
+    if (counter > SHOW_REPOS) {
+      break;
     }
-    destinationNode.appendChild(tmpFragment);
+  }
+  destinationNode.appendChild(tmpFragment);
 };
 if (!cachedPayload) {
-    fetch(API_DESTINATION)
-        .then(response => response.json())
-        .then(result => {
-            renderMultipleRepo(result);
-            localStorageWithExpiration.set(CACHE_KEY, result, GITHUB_CACHE_SECONDS);
-        });
+  fetch(API_DESTINATION)
+    .then((response: Response) => response.json())
+    .then((result: Object) => {
+      renderMultipleRepo(result);
+      localStorageWithExpiration.set(CACHE_KEY, result, GITHUB_CACHE_SECONDS);
+    });
 } else {
-    renderMultipleRepo(cachedPayload);
+  renderMultipleRepo(cachedPayload);
 }
