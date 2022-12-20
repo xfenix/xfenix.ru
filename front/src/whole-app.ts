988c477d6bda985f8ccb698dec9cbd77eaea8b5f
Denis Anikin <ad@xfenix.ru>
2022-12-20 04:34:56 +0300

    Fixed all typescript mess for now

--- front/src/whole-app.ts
+++ front/src/whole-app.ts
@@ -48,13 +48,18 @@ $(".top-head__menulink, .top-head__logolink").on(
   "click",
   function (eventObj: Event) {
     eventObj.preventDefault();
-    const originalHref: string = $(this).attr("href");
-    window.scrollTo({
-      top: originalHref.replace("#", "")
-        ? $(originalHref).offset().top - $(".top-head").height()
-        : 0,
-      behavior: "smooth",
-    });
+    const originalHref: string | null = $(this).attr("href");
+    if (originalHref !== null) {
+      const possibleOffset = $(originalHref).offset();
+      if (possibleOffset !== undefined) {
+        window.scrollTo({
+          top: originalHref.replace("#", "")
+            ? possibleOffset.top - $(".top-head").height()
+            : 0,
+          behavior: "smooth",
+        });
+      }
+    }
   }
 );
 
@@ -83,9 +88,11 @@ const API_KEY = "xfenix-apiaddr";
 const BYPASS_KEY = "xfenix-bypasscache";
 const GITHUB_CACHE_SECONDS = 12 * 3600;
 const SHOW_REPOS = 6;
-const API_DESTINATION = localStorage.getItem(API_KEY)
-  ? localStorage.getItem(API_KEY)
-  : "/api/githubrepos/";
+const API_DESTINATION = String(
+  localStorage.getItem(API_KEY)
+    ? localStorage.getItem(API_KEY)
+    : "/api/githubrepos/"
+);
 const localStorageWithExpiration = {
   set: (cacheKey: string, inputValue: Object, ttlSeconds: number): void => {
     localStorage.setItem(
@@ -100,25 +107,29 @@ const localStorageWithExpiration = {
     if (localStorage.getItem(BYPASS_KEY)) {
       return null;
     }
-    const storageData: { value: Object; expires_at: number } = JSON.parse(
-      localStorage.getItem(cacheKey.toString())
-    );
-    if (storageData !== null) {
-      if (
-        storageData.expires_at !== null &&
-        storageData.expires_at < new Date().getTime()
-      ) {
-        localStorage.removeItem(cacheKey.toString());
-      } else {
-        return storageData.value;
-      }
+    const fetchedData = localStorage.getItem(cacheKey.toString());
+    if (fetchedData === null) {
+      return null;
+    }
+    const storageData: { value: Object; expires_at: number } =
+      JSON.parse(fetchedData);
+    if (storageData === null) {
+      return null;
+    }
+    if (
+      storageData.expires_at !== null &&
+      storageData.expires_at < new Date().getTime()
+    ) {
+      localStorage.removeItem(cacheKey.toString());
+    } else {
+      return storageData.value;
     }
     return null;
   },
 };
 const cachedPayload = localStorageWithExpiration.get(CACHE_KEY);
 const destinationNode = document.querySelector(".github-wannabe");
-const templateElement: HTMLTemplateElement = document.querySelector(
+const templateElement: HTMLTemplateElement | null = document.querySelector(
   "#github-wannabe-tpl"
 );
 const renderOneRepo = (oneRepoPayload: {
@@ -129,53 +140,71 @@ const renderOneRepo = (oneRepoPayload: {
   forks_count: number;
   language: string;
 }) => {
-  const newNode: HTMLElement = templateElement.content.cloneNode(
+  const newNode: HTMLElement = templateElement?.content.cloneNode(
     true
   ) as HTMLElement;
-  const titleNode: HTMLAnchorElement = newNode.querySelector(
+  const titleNode: HTMLAnchorElement | null = newNode.querySelector(
     ".github-wannabe__title > a"
   );
-  const starsNode: HTMLElement = newNode.querySelector(
+  const starsNode: HTMLElement | null = newNode.querySelector(
     ".github-wannabe__stars"
   );
-  const forkNode: HTMLElement = newNode.querySelector(".github-wannabe__fork");
-  const languageNode: HTMLElement = newNode.querySelector(
+  const forkNode: HTMLElement | null = newNode.querySelector(
+    ".github-wannabe__fork"
+  );
+  const languageNode: HTMLElement | null = newNode.querySelector(
     ".github-wannabe__language"
   );
 
-  titleNode.textContent = oneRepoPayload.name;
-  titleNode.href = oneRepoPayload.html_url;
-
-  newNode.querySelector(".github-wannabe__descr").textContent =
-    oneRepoPayload.description;
-
-  starsNode.textContent = String(oneRepoPayload.stargazers_count);
-  (
-    starsNode.parentNode as HTMLAnchorElement
-  ).href = `${oneRepoPayload.html_url}/stargazers`;
-
-  const forkParent = forkNode.parentNode as HTMLAnchorElement;
-  if (oneRepoPayload.forks_count > 0) {
-    forkNode.textContent = String(oneRepoPayload.forks_count);
-    forkParent.href = `${oneRepoPayload.html_url}/network/members`;
-  } else {
-    forkParent.remove();
+  if (titleNode) {
+    titleNode.textContent = oneRepoPayload.name;
+    titleNode.href = oneRepoPayload.html_url;
   }
 
-  languageNode
-    .querySelector(".github-repo-color")
-    .classList.add(
-      "github-repo-color_" +
-        (oneRepoPayload.language
-          ? oneRepoPayload.language
-          : "nolanguage"
-        ).toLowerCase()
+  if (newNode) {
+    const innerNode: HTMLElement | null = newNode.querySelector(
+      ".github-wannabe__descr"
     );
-  languageNode.appendChild(
-    document.createTextNode(
-      oneRepoPayload.language ? oneRepoPayload.language : "No language"
-    )
-  );
+    if (innerNode) {
+      innerNode.textContent = oneRepoPayload.description;
+    }
+  }
+
+  if (starsNode && starsNode.textContent && starsNode.parentNode) {
+    starsNode.textContent = String(oneRepoPayload.stargazers_count);
+    (
+      starsNode.parentNode as HTMLAnchorElement
+    ).href = `${oneRepoPayload.html_url}/stargazers`;
+  }
+
+  if (forkNode) {
+    const forkParent = forkNode.parentNode as HTMLAnchorElement;
+    if (oneRepoPayload.forks_count > 0) {
+      forkNode.textContent = String(oneRepoPayload.forks_count);
+      forkParent.href = `${oneRepoPayload.html_url}/network/members`;
+    } else {
+      forkParent.remove();
+    }
+  }
+
+  if (languageNode) {
+    const colorNode: HTMLElement | null =
+      languageNode.querySelector(".github-repo-color");
+    if (colorNode) {
+      colorNode.classList.add(
+        "github-repo-color_" +
+          (oneRepoPayload.language
+            ? oneRepoPayload.language
+            : "nolanguage"
+          ).toLowerCase()
+      );
+    }
+    languageNode.appendChild(
+      document.createTextNode(
+        oneRepoPayload.language ? oneRepoPayload.language : "No language"
+      )
+    );
+  }
 
   return newNode;
 };
@@ -189,7 +218,9 @@ const renderMultipleRepo = (multiplePayload) => {
       break;
     }
   }
-  destinationNode.appendChild(tmpFragment);
+  if (destinationNode) {
+    destinationNode.appendChild(tmpFragment);
+  }
 };
 if (!cachedPayload) {
   fetch(API_DESTINATION)
